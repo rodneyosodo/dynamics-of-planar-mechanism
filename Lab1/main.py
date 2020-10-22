b414295ff3a0d1713f917485f6b92286357d7344
0x6f736f646f <blackd0t@protonmail.com>
2020-10-22 13:57:25 +0300

    👌 IMPROVE: Space the functions

--- Lab1/main.py
+++ Lab1/main.py
@@ -9,12 +9,14 @@ def get_precision_points(lower_limit: float, upper_limit: float, no_pp: int):
     :param lower_limit: The lower limit for teh range
     :param upper_limit: The upper limit for the range
     :param no_pp; The number of precision precision points
-    :return precision_points: Ther precision points determined
+    :return precision_points: The precision points determined
     """
     precision_points = []
     for i in range(1, no_pp + 1, 1):
-        precision_points.append((0.5 * (lower_limit + upper_limit)) - (0.5 * (upper_limit - lower_limit) * cos((pi * (2*i -1)) / (2 * no_pp))))
+        precision_points.append((0.5 * (lower_limit + upper_limit)) - (
+                    0.5 * (upper_limit - lower_limit) * cos((pi * (2 * i - 1)) / (2 * no_pp))))
     return precision_points
+
 
 def get_theta_4(theta2: list):
     """"
@@ -29,17 +31,19 @@ def get_theta_4(theta2: list):
         theta4.append(m)
     return theta4
 
+
 def convert_angles_to_radians(angles: list):
     """"
     Converts a list of angles from degrees to radians
     :param angles: The list of angles
     :return new_angles: The converted angles
     """
-    
+
     new_angles = []
     for i in angles:
         new_angles.append(radians(i))
     return new_angles
+
 
 def compute_freudensteins_constants(input_angles: list, output_angles: list):
     """
@@ -57,27 +61,30 @@ def compute_freudensteins_constants(input_angles: list, output_angles: list):
     # Define coefficient matrices as numpy arrays
     A = np.array([
         [cos(output_angles[0]),
-        -1 * cos(input_angles[0]),
-        1
-        ],
+         -1 * cos(input_angles[0]),
+         1
+         ],
         [cos(output_angles[1]),
-        -1 * cos(input_angles[1]),
-        1
-        ],
+         -1 * cos(input_angles[1]),
+         1
+         ],
         [cos(output_angles[2]),
-        -1 * cos(input_angles[2]),
-        1]])
+         -1 * cos(input_angles[2]),
+         1]])
     # Define results matrices as numpy arrays
     B = np.array([
         cos(input_angles[0] - output_angles[0]),
         cos(input_angles[1] - output_angles[1]),
         cos(input_angles[2] - output_angles[2])])
     # Use numpy’s linear algebra solve function to solve the system
-    constants = np.linalg.solve(A,B)
+    constants = np.linalg.solve(A, B)
     return constants
 
-def lenghts_of_links(constants: list):
+
+def lengths_of_links(constants: list):
     """
+    Compute the lengths of the links
+    :param list: The constants
     k1 = d / a; a = d / k1
     k2 = d / c; c = d / k2
     k3 = (a*a - b*b + c*c + d*d) / 2ac; b = squareroot(a^2 + c^2 + d^2 - 2ack3)
@@ -85,8 +92,9 @@ def lenghts_of_links(constants: list):
     d = 410
     a = d / constants[0]
     c = d / constants[1]
-    b = sqrt((a*a) + (c*c) + (d*d) - (2*a*c*constants[2]))
+    b = sqrt((a * a) + (c * c) + (d * d) - (2 * a * c * constants[2]))
     return a, b, c, d
+
 
 def get_transmission_angles(a, b, c, d, lower_limit, upper_limit, steps):
     """
@@ -102,9 +110,10 @@ def get_transmission_angles(a, b, c, d, lower_limit, upper_limit, steps):
     """
     transmission_angles = []
     for i in range(lower_limit, upper_limit, steps):
-        m = acos(((b*b + c*c) - (a*a + d*d) + (2*a*d*cos(i))) / (2 * b * c))
+        m = acos(((b * b + c * c) - (a * a + d * d) + (2 * a * d * cos(i))) / (2 * b * c))
         transmission_angles.append(m)
     return transmission_angles
+
 
 def plot_transmission_angles_and_input_angles(transmission_angles: list, input_angles: list):
     """
@@ -115,17 +124,18 @@ def plot_transmission_angles_and_input_angles(transmission_angles: list, input_a
     plt.xlabel("Input angles")
     plt.ylabel("Transmission angles")
     plt.title("Input angles vs Transmission angles")
-    plt.plot(input_angles, transmission_angles, color='red', linewidth=1.0,)
+    plt.plot(input_angles, transmission_angles, color='red', linewidth=1.0, )
     plt.show()
 
-    
+
 if __name__ == "__main__":
     theta2 = get_precision_points(15, 165, 3)
     theta4 = get_theta_4(theta2)
     constants = compute_freudensteins_constants(theta2, theta4)
     print("The constants are:\nk1: {}\nk2: {}\nk3: {}\n".format(constants[0], constants[1], constants[2]))
-    a, b, c, d = lenghts_of_links(constants)
-    print("The lenghts of links are:\nInput link: {}\nCoupler: {}\nOutput link: {}\nFixed link: {}\n".format(a,b,c,d))
+    a, b, c, d = lengths_of_links(constants)
+    print(
+        "The lenghts of links are:\nInput link: {}\nCoupler: {}\nOutput link: {}\nFixed link: {}\n".format(a, b, c, d))
     transmission_angles = get_transmission_angles(a, b, c, d, 15, 165, 5)
     print("The transmission angles for the range of inputs are:\n{}\n".format(transmission_angles))
     plot_transmission_angles_and_input_angles(transmission_angles, [i for i in range(15, 165, 5)])
